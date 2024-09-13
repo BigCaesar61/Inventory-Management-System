862fb9ed3cedba162366fafdf2e383406758a1ee
Martin Leone <martinnikolaslll@gmail.com>
2024-09-12 20:17:35 -0400

    Initialize inventory with product objects

---
+++ Inventory_Management_System.js
@@ -0,0 +1,12 @@
+//Task 1: Create an Inventory Array of Product Objects
+
+let inventory = [
+
+{name: "Football", price: 26.50, quantity: 200, lowStockLevel: 25},
+{name: "Baseball", price: 5.99, quantity: 200, lowStockLevel: 25},
+{name: "Soccerball", price: 12.00, quantity: 200, lowStockLevel: 25},
+{name: "Cleats", price: 45.99, quantity: 500, lowStockLevel: 100},
+{name: "Gloves", price: 15.00, quantity: 500, lowStockLevel: 100},
+
+];
+console.log(inventory);
